480b341139561571cc3e2fa5376975068fc2b645
JaneChelle <2962784374@qq.com>
2020-06-07 17:09:39 +0800

    注册完成

--- src/views/sign-up.vue
+++ src/views/sign-up.vue
@@ -10,7 +10,7 @@
           <h2>欢迎!<br>
             <span class="rignt_tips">在下面输入您的详细信息注册</span>
           </h2>
-          <form 
+          <!-- <form 
             id="app"
             @submit="checkFrom"
             action="https://vuejs.org/"
@@ -28,7 +28,25 @@
             <div class="ipt">密码：<input id="password" name="password" type="password" v-model="password"></div>
             <div class="ipt">密码确认：<input id="r_password" name="r_password" type="password" v-model="r_password"></div>
             <input type="submit" value="提交" class="submit">
-          </form>
+          </form> -->
+        <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="电子邮件" prop="email">
+                <el-input  v-model="ruleForm.email" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="手机号" prop="phone">
+                <el-input v-model="ruleForm.phone" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="pass">
+                <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="确认密码" prop="checkPass">
+                <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                <el-button @click="resetForm('ruleForm')">重置</el-button>
+            </el-form-item>
+        </el-form>
       </div>
     </div>
   </div>
@@ -36,49 +54,132 @@
 
 <script>
 // import aesjs from 'aes-js';
-export default {
-  data(){
-    return {
-      errors:[],
-      email: '',
-      phone: '',
-      password: '',
-      r_password: ''
+ import BaseUrl from '../api/index';
+ export default {
+    data() {
+     var checkPhone = (rule, value, callback) => {
+    const phoneReg = /^1[3|4|5|7|8][0-9]{9}$/
+    if (!value) {
+      return callback(new Error('电话号码不能为空'))
     }
-  },
-  methods: {
-    checkForm: function(){
-      this.errors = [];
-      if(!this.email){
-        this.errors.push('邮件不能为空')
-      } else if(!this.validEmail(this.email)){
-         this.errors.push('请输入有效的电子邮件')
+    setTimeout(() => {
+      // Number.isInteger是es6验证数字是否为整数的方法,实际输入的数字总是识别成字符串
+      // 所以在前面加了一个+实现隐式转换
+ 
+      if (!Number.isInteger(+value)) {
+        callback(new Error('请输入数字值'))
+      } else {
+        if (phoneReg.test(value)) {
+          callback()
+        } else {
+          callback(new Error('电话号码格式不正确'))
+        }
       }
-      if(!this.phone){
-        this.errors.push("手机号不能为空")
-      } else if(!this.vaildPhone(this.phone)){
-         this.errors.push("请输入有效手机号")
+    }, 100)
+  }
+  var checkEmail = (rule, value, callback) => {
+    const mailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
+    if (!value) {
+      return callback(new Error('邮箱不能为空'))
+    }
+    setTimeout(() => {
+      if (mailReg.test(value)) {
+        callback()
+      } else {
+        callback(new Error('请输入正确的邮箱格式'))
       }
-      if(!this.password){
-        this.errors.push('密码不能为空');
-      }
-      if(!this.r_password){
-        this.errors.push("请再次输入密码")
-      }
-      if(this.password != this.r_password){
-        this.errors.push("两次密码不一致")
-      }
+    }, 100)
+  }
+
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        ruleForm: {
+          email: '',
+          phone: '',
+          pass: '',
+          checkPass: ''
+        },
+        rules: {
+          phone: [
+            { validator: checkPhone, trigger: 'blur' }
+          ],
+          email: [
+            { validator: checkEmail, trigger: 'blur' }
+          ],
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+        }
+      };
     },
-    validEmail: function (email) {
-      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(email);
-    },
-    vaildPhone: function(phone){
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+           alert('submit!');
+            let v = this;
+            this.$axios({
+              method: 'post',
+              url: BaseUrl + '/student/register',
+              data: {
+                'email': v.ruleForm.email,
+                'phone': v.ruleForm.phone,
+                'password': v.ruleForm.pass
+              }
+            }).then(function(res){
+              if(res.data.code == 1){
+                v.$message({
+                  message: '注册成功',
+                  type: 'success',
+                  showClose: true
+                })
+              } else{
+                v.$message({
+                  message: res.data.msg,
+                  showClose: true
+                })
+              }
+            })
+          } else {
+            console.log('提交失败');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
+      validEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+     },
+    vaildPhone(phone){
       var ph = /^1[3456789]\d{9}$/
       return ph.test(phone);
     }
+    }
   }
-}
 </script>
 
 <style lang="stylus">
